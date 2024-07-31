99eed785b9421b7e82edc89d8f5e379244e8ba36
Alv4r0Dev13 <alvarorenancf00@gmail.com>
2024-07-31 20:25:21 -0300

    uploading base files

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from './styles';
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+      <Header>Homepage</Header>
+    </div>
+  )
+}
+
+export default Homepage;
