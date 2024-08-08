b733a05221198b3c2fe6c7def630490416c7955a
Alv4r0Dev13 <alvarorenancf00@gmail.com>
2024-08-07 22:43:16 -0300

    remove comment

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'http://192.168.0.111:3001',
   baseURL: `${process.env.REACT_APP_API_URL}`,
   headers: { 'Access-Control-Allow-Origin': '*' },
 });
